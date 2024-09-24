cd08c03e8c0629bcbce900b575f3eca3908920fd
Rahmonbek <madaminovrahmonbek@gmail.com>
2024-09-24 15:46:46 +0500

    update

--- testimonials-grid-section-main/tailwind.config.js
+++ testimonials-grid-section-main/tailwind.config.js
@@ -1,22 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {
-      backgroundColor: {
+      colors:{
         "moderate-violet":"hsl(263, 55%, 52%)",
-        "very-dark-grayish-blue":"hsl(217, 19%, 35%)",
-        "very-dark-blackish-blue":"hsl(219, 29%, 14%)",
+        "dark-grayish-blue":"hsl(217, 19%, 35%)",
+        "dark-blackish-blue":"hsl(219, 29%, 14%)",
         "light-gray":"hsl(0, 0%, 81%)",
-        "light-grayish-blue":"hsl(210, 46%, 95%)"
+        "light-grayish-blue":"hsl(210, 46%, 95%)",
       },
-      textColor: {
-        "moderate-violet":"hsl(263, 55%, 52%)",
-        "very-dark-grayish-blue":"hsl(217, 19%, 35%)",
-        "very-dark-blackish-blue":"hsl(219, 29%, 14%)",
-        "light-gray":"hsl(0, 0%, 81%)",
-        "light-grayish-blue":"hsl(210, 46%, 95%)"
-      }
     },
   },
   plugins: [],
