2998889219604a83b60349ec5610de92378a7af6
Rahmonbek <madaminovrahmonbek@gmail.com>
2024-09-30 15:14:39 +0500

    newsletter-sign-up

---
+++ newsletter-sign-up-with-success-message-main/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        "tomato":"hsl(4, 100%, 67%)",
+        "ark-slate-grey":"hsl(234, 29%, 20%)",
+        "charcoal-grey":"hsl(235, 18%, 26%)",
+        "grey":"hsl(231, 7%, 60%)",
+      }
+    },
+  },
+  plugins: [],
+}
