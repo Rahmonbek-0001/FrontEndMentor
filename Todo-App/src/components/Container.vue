1d227878c85901409c54e3b533ae953770bb1a56
Rahmonbek-0001 <madaminovrahmonbek@gmail.com>
2024-11-18 16:45:59 +0500

    Advice-generator-submited

--- Todo-App/src/components/Container.vue
+++ Todo-App/src/components/Container.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="w-full max-w-xl mx-auto px-4">
-		<slot />
-	</div>
+  <div class="w-full max-w-xl mx-auto px-4 -mb-20">
+    <slot />
+  </div>
 </template>
--- age-calculator/script.js
+++ age-calculator/script.js
@@ -1,5 +1,5 @@
 function age() {
-  let input = document.getElementsByTagName("input")
+  let input = document.getElementsByTagName("input");
   let d1 = document.getElementById("date");
   let m1 = document.getElementById("month").value;
   let y1 = document.getElementById("year").value;
@@ -8,8 +8,14 @@ function age() {
   let m2 = 1 + date.getMonth();
   let y2 = date.getFullYear();
   let month = [31, 28, 31, 30, 31, 30, 31, 31, 31, 30, 31, 30];
-  if (d1.value > d2) { d2 = d2 + month[m2 - 1]; m2 = m2 - 1; }
-  if (m1 > m2) { m2 = m2 + 12; y2 = y2 - 1; }
+  if (d1.value > d2) {
+    d2 = d2 + month[m2 - 1];
+    m2 = m2 - 1;
+  }
+  if (m1 > m2) {
+    m2 = m2 + 12;
+    y2 = y2 - 1;
+  }
   let d = d2 - d1.value;
   let m = m2 - m1;
   let y = y2 - y1;
@@ -17,8 +23,8 @@ function age() {
     `<div style="display: flex; align-items: center; height: inherit;"><h2 id="ages">${y}-</h2><h3 class="ages">Year</h3></div>` +
     `<div style="display: flex; gap: 15px; align-items: center; height: inherit;"><h2 id="ages">${m}-</h2><h3 class="ages">Month </h3></div>` +
     `<div style="display: flex; align-items: center; height: inherit;"><h2 id="ages">${d}-</h2><h3 class="ages">Day</h3></div>`;
-  if (y1 = '0') {
-    `<h2 id="" style="color: #fff;">${y}-</h2>`
-    console.log(y,'y',y1,'y1');
+  if ((y1 = "0")) {
+    `<h2 id="" style="color: #fff;">${y}-</h2>`;
+    console.log(y, "y", y1, "y1");
   }
 }
